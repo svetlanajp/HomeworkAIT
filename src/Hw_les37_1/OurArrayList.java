c97b855e49abef5b1638008513473d7338a10e36
AIT TR Student <svetlanajp2013@mail.com>
2024-01-15 11:39:18 +0100

    OurArrayList: method remove

--- src/Hw_les37_1/OurArrayList.java
+++ src/Hw_les37_1/OurArrayList.java
@@ -48,7 +48,7 @@ public class OurArrayList<E> implements OurList<E> {
 
 
     public boolean remove(E value) {
-        for (int i = 0; i < source.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (source[i].equals(value)) {
                 removeById(i);
                 return true;
